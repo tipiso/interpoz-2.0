4ff422d977f971fbdfe25f16b2a1f32ad3d0b012
artur.sokalski <artur.sokalski@zaven.pl>
2022-10-24 13:57:52 +0200

    temporary header solution

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,30 @@
 import * as React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import NavItem from "react-bootstrap/NavItem";
+import Nav from 'react-bootstrap/Nav';
+
+import Logo from '../../images/logo.svg';
 
 import "./Layout.scss";
 
 export default function Layout({ children }) {
-    return <div>{children}</div>
+    return (
+        <div>
+            <Navbar>
+                <NavbarBrand href='/'>
+                    <img src={Logo} alt='Logo icon' />
+                </NavbarBrand>
+                <Nav navbar>
+                    <NavItem>O firmie</NavItem>
+                    <NavItem>Kontakt</NavItem>
+                    <NavItem>Oferta</NavItem>
+                </Nav>
+            </Navbar>
+            <Container fluid className='px-0'>
+                {children}
+            </Container>
+        </div>
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,51 @@
 import * as React from "react"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import Layout from "../components/Layout";
+import Extinguisher from '../images/extinguisher-img.png';
+import RedSign from '../images/red-sign-img.png';
+import GreenSign from '../images/green-sign-img.png';
 
 import "./Home.scss";
 
 const IndexPage = () => {
   return (
-      <Layout>test</Layout>
+      <Layout>
+        <header className='header'>
+            <Row>
+                <Col xs={6} as='h1' className='header__text'>
+                    <div title='Usługi'>
+                        <span>Usługi</span>
+                    </div>
+                    <div title='PPOŻ'>
+                        <span>PPOŻ</span>
+                    </div>
+                    <div title='i BHP'>
+                        <span>i BHP</span>
+                    </div>
+                </Col>
+                <Col className='images' xs={6}>
+                    <img src={Extinguisher} alt='extinguisher' />
+                    <img src={GreenSign} alt='green sign' />
+                    <img src={RedSign} alt="red sign" />
+                </Col>
+            </Row>
+        </header>
+          <div className='header header--background'>
+              <div className='header__text'>
+                  <div title='Usługi'>
+                      <span>Usługi</span>
+                  </div>
+                  <div title='PPOŻ'>
+                      <span>PPOŻ</span>
+                  </div>
+                  <div title='i BHP'>
+                      <span>i BHP</span>
+                  </div>
+              </div>
+          </div>
+      </Layout>
   )
 }
 
