1645e2a2400f08677ed9b3c6ee526d9245b452b3
artur.sokalski <artur.sokalski@zaven.pl>
2022-10-25 15:40:11 +0200

    added all desktop layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
@@ -6,6 +8,7 @@ import NavItem from "react-bootstrap/NavItem";
 import Nav from 'react-bootstrap/Nav';
 
 import Logo from '../../images/logo.svg';
+import FooterLogo from "../../images/footer-logo.svg";
 
 import "./Layout.scss";
 
@@ -19,12 +22,33 @@ export default function Layout({ children }) {
                 <Nav navbar>
                     <NavItem>O firmie</NavItem>
                     <NavItem>Kontakt</NavItem>
-                    <NavItem>Oferta</NavItem>
+                    <NavItem as='button' className='red-btn'>Oferta</NavItem>
                 </Nav>
             </Navbar>
-            <Container fluid className='px-0'>
+
+            <Container className='overflow-hidden' fluid>
                 {children}
             </Container>
+
+            <footer className='footer'>
+                <Col xs={5}>
+                    <img src={FooterLogo} alt='footer logo' />
+                    <ul className='footer__list'>
+                        <li>Robert Bujny Interpoż</li>
+                        <li>ul. Zdrojowa 32</li>
+                        <li>28-100 Busko-Zdrój</li>
+                    </ul>
+                </Col>
+                <Col xs={6}>
+                    <Navbar className='footer__navbar'>
+                        <Nav navbar>
+                            <NavItem>O firmie</NavItem>
+                            <NavItem>Kontakt</NavItem>
+                            <NavItem>Oferta</NavItem>
+                        </Nav>
+                    </Navbar>
+                </Col>
+            </footer>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import * as React from "react";
+import { useState } from "react";
 import Col from "react-bootstrap/Col";
 import {Collapse, Row} from "react-bootstrap";
 
@@ -7,11 +8,24 @@ import Extinguisher from '../images/extinguisher-img.png';
 import RedSign from '../images/red-sign-img.png';
 import GreenSign from '../images/green-sign-img.png';
 import ScrollTo from '../images/scroll-to-icon.svg';
+import ExtinguisherOnWall from '../images/extinguisher-on-wall.png';
+import MailIcon from '../images/mail-icon.svg'
+import PhoneIcon from "../images/phone-icon.svg";
 import {services} from "../constants/constants";
 
 import "./Home.scss";
 
 const IndexPage = () => {
+    const [openedCollapse, setOpenedCollapse] = useState({});
+
+    const toggleCollapse = (uniqueKey) => {
+        if(!openedCollapse[uniqueKey]) {
+            setOpenedCollapse({[uniqueKey]: true})
+        }else {
+            setOpenedCollapse({[uniqueKey]: false})
+        }
+    }
+
   return (
       <Layout>
             <Row as='header' className='header'>
@@ -27,20 +41,22 @@ const IndexPage = () => {
                     <img className='image image--green-sign' src={GreenSign} alt='green sign' />
                     <img className='image image--red-sign' src={RedSign} alt="red sign" />
                 </Col>
-                <div className='header--background'>
+                <Col xs={6} className='header--background'>
                     <div className='header__text'>
                         <span>Usługi</span>
                         <span>PPOŻ</span>
                         <span>i BHP</span>
                     </div>
-                </div>
+                </Col>
             </Row>
 
 
-          <section className='subheading'>
-              <p className='subheading__text'>Zapewniamy najwyższe standardy z zakresu <br /> bezpieczeństwa pożarowego i BHP</p>
-              <button className='subheading__btn'><img src={ScrollTo} alt='scroll down icon' /></button>
-          </section>
+          <Row as='section'>
+              <Col className='subheading' xs={12}>
+                  <p className='subheading__text'>Zapewniamy najwyższe standardy z zakresu <br /> bezpieczeństwa pożarowego i BHP</p>
+                  <button><img src={ScrollTo} alt='scroll down icon' /></button>
+              </Col>
+          </Row>
 
 
           <Row as='section' className='services'>
@@ -51,23 +67,54 @@ const IndexPage = () => {
                       <span>usług</span>
                   </div>
               </Col>
-              <div className='services--background'>
+              <Col xs={6} className='services--background'>
                   <div className='header__text'>
                       <span>Zakres</span>
                       <span>naszych</span>
                       <span>usług</span>
                   </div>
-              </div>
+              </Col>
 
-              <Col xs={6}>
-                  {services.map((service, idx) => (
-                      <div key={service.title + idx}>
-                          <span>{service.title}</span>
-                          <Collapse>
-                              <div>{service.content}</div>
+              <Col className='services__list' xs={6}>
+                  {services.map((service, idx) => {
+                      const uniqueKey = service.title + idx;
+
+                   return (
+                       <div className='service' key={uniqueKey}>
+                          <span className='service__title' onClick={() => toggleCollapse(uniqueKey)}>{service.title}</span>
+                          <Collapse key={uniqueKey + '-collapse'} in={openedCollapse[uniqueKey]}>
+                              <div className='service__content'>{service.content}</div>
                           </Collapse>
-                      </div>
-                  ))}
+                      </div>)
+                  })}
+              </Col>
+          </Row>
+
+          <Row className='about' as='section'>
+              <Col className='ps-0' xs={6}>
+                  <img src={ExtinguisherOnWall} alt='extinguisher on wall' />
+              </Col>
+              <Col className='about__text' xs={6}>
+                  <h4 className='about__heading'>O firmie</h4>
+                  <p className='about__paragraph'>Interpoż działa prężnie od 2019 roku,  firmata została stworzona z pasji przez ekspertów powiązanych z daną dziedziną wieloletnim doświadczeniem. </p>
+                  <p className='about__paragraph'>Jako firma jak i specjaliści zapewniamy największy standard usług w zależności od potrzeb, wyżej wymienione zakresy naszego działania mogą być elastycznie modyfikowane w zależności co Państwo potrzebują.</p>
+                  <p className='about__paragraph'>Poniżej przedstawiamy plik zawierający dane dotyczące oferowanych przykładowych usług jakie możemy Państwu zapewnić, w razie niepewności zapraszamy do kontaktu.</p>
+                  <button className='red-btn'>Oferta</button>
+              </Col>
+          </Row>
+
+          <Row as='section' className='contact'>
+              <Col className='contact__heading' xs={6} as='h5'>Kontakt</Col>
+              <Col className='contact__content' xs={6}>
+                  <div className='d-flex flex-column'>
+                      <p className='contact__info'>Jeśli masz więcej pytań na temat usług zostaw nam swoje dane <br /> kontaktowe, oddzwonimy i chętnie odpowiemy na Twoje pytania.</p>
+                      <a className='contact__anchor' href='tel:+48691511756'>
+                          <img src={PhoneIcon} alt='phone icon' /> 691-511-756
+                      </a>
+                      <a className='contact__anchor' href='mailto:kontakt@interpoz.com'>
+                          <img src={MailIcon} alt='Mail icon' /> kontakt@interpoz.com
+                      </a>
+                  </div>
               </Col>
           </Row>
       </Layout>
