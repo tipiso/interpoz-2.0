54894f0176b28bb1fb3e48f6a715ad12f47ea4cd
artur.sokalski <artur.sokalski@zaven.pl>
2022-10-25 10:30:20 +0200

    added section services and subheading

---
+++ src/constants/constants.js
@@ -0,0 +1,24 @@
+const services = [
+    {
+        title: 'Szkolenia PPOŻ i BHP',
+        content: 'Wyspecjalizowana kadra przepowadzi odpowiednie szkolenia dostosowane do zaistniałych potrzeb'
+    },
+    {
+        title: 'Instrukcje bezpieczeństwa',
+        content: 'Wyspecjalizowana kadra przepowadzi odpowiednie szkolenia dostosowane do zaistniałych potrzeb'
+    },
+    {
+        title: 'Dokumentacja PPOŻ',
+        content: 'Przygotowanie dokumentacji do kontroli oraz budynków do odbioru'
+    },
+    {
+        title: 'Ochrona PPOŻ i BHP',
+        content: 'Zapewniamy najwyższą ochronę dla firm w temacie PPOŻ i BHP'
+    },
+    {
+        title: 'Przeglądy urządzeń',
+        content: 'Przegląd urządzeń przeciwpożarowych'
+    }
+];
+
+export { services };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,75 @@
 import * as React from "react"
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import {Collapse, Row} from "react-bootstrap";
 
 import Layout from "../components/Layout";
 import Extinguisher from '../images/extinguisher-img.png';
 import RedSign from '../images/red-sign-img.png';
 import GreenSign from '../images/green-sign-img.png';
+import ScrollTo from '../images/scroll-to-icon.svg';
+import {services} from "../constants/constants";
 
 import "./Home.scss";
 
 const IndexPage = () => {
   return (
       <Layout>
-        <header className='header'>
-            <Row>
+            <Row as='header' className='header'>
                 <Col xs={6} as='h1' className='header__text-wrap'>
                     <div className='header__text'>
-                        <div title='Usługi'>
-                            <span>Usługi</span>
-                        </div>
-                        <div title='PPOŻ'>
-                            <span>PPOŻ</span>
-                        </div>
-                        <div title='i BHP'>
-                            <span>i BHP</span>
-                        </div>
+                        <span>Usługi</span>
+                        <span>PPOŻ</span>
+                        <span>i BHP</span>
                     </div>
                 </Col>
                 <Col className='images' xs={6}>
-                    <img src={Extinguisher} alt='extinguisher' />
-                    <img src={GreenSign} alt='green sign' />
-                    <img src={RedSign} alt="red sign" />
+                    <img className='image image--extinguisher-sign' src={Extinguisher} alt='extinguisher' />
+                    <img className='image image--green-sign' src={GreenSign} alt='green sign' />
+                    <img className='image image--red-sign' src={RedSign} alt="red sign" />
                 </Col>
+                <div className='header--background'>
+                    <div className='header__text'>
+                        <span>Usługi</span>
+                        <span>PPOŻ</span>
+                        <span>i BHP</span>
+                    </div>
+                </div>
             </Row>
-        </header>
-          <div className='header header--background'>
-              <div className='header__text'>
-                  <div title='Usługi'>
-                      <span>Usługi</span>
+
+
+          <section className='subheading'>
+              <p className='subheading__text'>Zapewniamy najwyższe standardy z zakresu <br /> bezpieczeństwa pożarowego i BHP</p>
+              <button className='subheading__btn'><img src={ScrollTo} alt='scroll down icon' /></button>
+          </section>
+
+
+          <Row as='section' className='services'>
+              <Col xs={6} as='h2' className='services__text-wrap'>
+                  <div className='services__text'>
+                      <span>Zakres</span>
+                      <span>naszych</span>
+                      <span>usług</span>
                   </div>
-                  <div title='PPOŻ'>
-                      <span>PPOŻ</span>
-                  </div>
-                  <div title='i BHP'>
-                      <span>i BHP</span>
+              </Col>
+              <div className='services--background'>
+                  <div className='header__text'>
+                      <span>Zakres</span>
+                      <span>naszych</span>
+                      <span>usług</span>
                   </div>
               </div>
-          </div>
+
+              <Col xs={6}>
+                  {services.map((service, idx) => (
+                      <div key={service.title + idx}>
+                          <span>{service.title}</span>
+                          <Collapse>
+                              <div>{service.content}</div>
+                          </Collapse>
+                      </div>
+                  ))}
+              </Col>
+          </Row>
       </Layout>
   )
 }
